6cb67022d12a564b5e5fe3049d1ecbf1b46b9258
manasmishra203 <manas.25bcy10225.vitbhopal.ac.in>
2026-09-10 21:46:45 +0530

    added automation

--- qr.py
+++ qr.py
@@ -1,13 +1,23 @@
 import qrcode
-website_link = input("enter the URL: ") #'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-qr=qrcode.QRCode(
-    version=1,#defines the size of the QR code
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=5, #defines how many pixels each “box” of the QR code is
-    border=5, #defines how many boxes thick the border should be
-)
-qr.add_data(website_link)
-qr.make()
-#this saving image comes under pillow library
-img=qr.make_image(fill_color=input("enter color of qr: "),back_color=input("enter color of background: "))
-img.save(input("enter the name of the file to save qrcode with extension: ")) #'qrcode.png'
+
+number = int(input("Enter the number of QR codes you want to generate: "))
+
+qr_color = input("Enter color of QR: ")
+bg_color = input("Enter color of background: ")
+for i in range(number):
+
+    website_link = input("Enter the URL: ")
+
+    qr=qrcode.QRCode(
+        version=2,#defines the size of the QR code
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=5, #defines how many pixels each “box” of the QR code is
+        border=5, #defines how many boxes thick the border should be
+    )
+    qr.add_data(website_link)
+    qr.make()
+    #this saving image comes under pillow library
+
+    img=qr.make_image(fill_color=qr_color,back_color=bg_color)
+    img.save(input("enter the name of the file to save qrcode with extension: ")) #'qrcode.png'
+print("All QR codes generated successfully!")
