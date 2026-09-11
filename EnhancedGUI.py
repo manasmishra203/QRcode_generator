edb89ecc5bb6b0a60c4380977c9ac4ef9d7d0db3
manasmishra203 <manas.25bcy10225.vitbhopal.ac.in>
2026-09-12 02:02:42 +0530

    added styleingui

---
+++ EnhancedGUI.py
@@ -0,0 +1,54 @@
+import qrcode
+import tkinter as tk #for making gui
+
+def generate_qr():
+    website_link = entry.get()
+    qr_color = color_entry.get()
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=5,
+        border=5
+    )
+
+    qr.add_data(website_link)
+    qr.make()
+    img = qr.make_image(
+        fill_color=qr_color,
+        back_color="white"
+    )
+
+    img.save("qrcode.png")
+
+    print("QR code generated successfully!")
+
+window =tk.Tk()  #main app window
+window.title("QR Code Generator") #object.method() window->object title->method
+window.geometry("500x400")
+
+label = tk.Label(window, text="Enter URL: ")
+label.pack() #it put the widget into the window
+
+entry = tk.Entry(window,width=40)
+entry.pack()
+
+color_label = tk.Label(
+    window,
+    text="Enter QR color:"
+)
+
+color_label.pack()
+
+
+color_entry = tk.Entry(
+    window,
+    width=40
+)
+
+color_entry.pack()
+#command=generate_qr()-> run the dunction immediately
+#command=generate_qr-> tkinter run it laterwhen button is clicked
+button = tk.Button(window, text="Generate QR",command=generate_qr)
+button.pack()
+
+window.mainloop() #it creates window running
